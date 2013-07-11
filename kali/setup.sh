09cdab69870074ec09f3a0e4fbff058f50f5aa98
Lee Baird <leebaird@gmail.com>
2013-07-10 23:14:21 -0400

    Update setup.sh

--- kali/setup.sh
+++ kali/setup.sh
@@ -2,33 +2,32 @@
 
 clear
 
-echo -e "\e[1;34mSetting up aliases.\e[0m"
+echo
+echo
+
+echo -e "\e[1;33mSetting up aliases.\e[0m"
 cp /opt/scripts/kali/alias /root/.bash_aliases
 source /root/.bash_aliases
-
 echo
-echo -e "\e[1;34mInstalling Filezilla.\e[0m"
+
+echo -e "\e[1;33mInstalling arp-scan.\e[0m"
+apt-get install arp-scan
+echo
+
+echo -e "\e[1;33mInstalling Filezilla.\e[0m"
 apt-get install filezilla
-
 echo
-echo -e "\e[1;34mInstalling gedit.\e[0m"
+
+echo -e "\e[1;33mInstalling gedit.\e[0m"
 apt-get install gedit
-
 echo
-echo -e "\e[1;34mInstalling xdotool.\e[0m"
+
+echo -e "\e[1;33mInstalling recon-ng.\e[0m"
+apt-get install recon-ng
+echo
+
+echo -e "\e[1;33mInstalling xdotool.\e[0m"
 apt-get install xdotool
 
 echo
-echo -e "\e[1;34mInstalling arp-scan.\e[0m"
-apt-get install arp-scan
-
 echo
-echo
-
-echo
-echo -e "\e[1;34mInstalling recon-ng.\e[0m"
-apt-get install recon-ng
-
-echo
-echo
-
