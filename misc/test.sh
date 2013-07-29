f397337fb7e710821065ec5fdd03a4f6e082fdad
leebaird <leebaird@gmail.com>
2013-07-29 12:33:43 -0400

    Added new icons for reports.

--- misc/test.sh
+++ misc/test.sh
@@ -4,25 +4,21 @@ user=$(whoami)
 
 clear
 
-echo
-echo
-echo "Usage: target.com"
-echo
-echo -n "Domain: "
-read domain
-echo
-echo
+#echo
+#echo
+#echo "Usage: target.com"
+#echo
+#echo -n "Domain: "
+#read domain
+#echo
+#echo
 
-awk '{print $3}' /$user/$domain/data/records.txt | grep -v '[A-Za-z]' | grep -E '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' -o | sort -u > tmp
-awk '{print $2}' /$user/$domain/data/subdomains.txt | grep -v '[A-Za-z]' | sort -u > tmp2
-grep -v ':' /$user/$domain/data/zonetransfer.txt | grep -E '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' -o | sort -u > tmp3
-cat tmp tmp2 tmp3 | sort -n -u -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 | sed '/^$/d' > zhosts
+sed 's/Pass/<center><img src="..\/images\/pass.jpeg" height="50" width="50"><\/center>/g; 
+s/Fail/<center><img src="..\/images\/fail.jpeg" height="50" width="50"><\/center>/g; 
+s/ class="info"//g; s/ class="rfail"//g; s/ class="rinfo"//g; s/ class="rpass"//g; s/ class="rsecu"//g;
+s/ class="rwarn"//g' tmp > /root/nwfcu.org/data/tmp2
 
-wc -l tmp
-wc -l tmp2
-wc -l tmp3
-wc -l zhosts
-
+firefox /root/nwfcu.org/data/tmp2 &
 echo
 echo
 
