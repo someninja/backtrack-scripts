6c38f7e74ecbed8f4f0a6e35b4fe0653394bcfd4
Lee <leebaird@gmail.com>
2013-08-09 13:06:46 -0400

    .

--- misc/test.sh
+++ misc/test.sh
@@ -20,14 +20,3 @@ echo
      x=$(ls -l | grep 'robtex' | awk '{print $5,$8}' | sort | head -1 | awk '{print $2}')
      mv $x tmp
 
-     sed '/<!DOCTYPE html>/,/<div id="c0a">/d' tmp | sed '/nopad sortable nospan/,/<\/html>/d' | sed '/<div id="c0b1">/,/DNS Records/d' | sed 's/<h2 class="h2s">Graph<\/h2>//g; s/<h2 class="h2s">Shared<\/h2>//g; s/"7.00"/"10.00"/g; s/"9.00"/"12.00"/g' > tmp2
-
-     echo "          </div>" >> tmp2
-     echo "     </div>" >> tmp2
-     echo "</div>" >> tmp2
-     echo "" >> tmp2
-     echo "</body>" >> tmp2
-     echo "" >> tmp2
-     echo "</html>" >> tmp2
-
-     cat tmp2 >> /$user/$domain/pages/robtex.htm
